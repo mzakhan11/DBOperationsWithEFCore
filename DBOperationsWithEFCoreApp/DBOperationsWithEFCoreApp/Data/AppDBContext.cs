137e79a8104d2d0e18c49195fe3ed798053531aa
unsafareed <Unsafareed123@gmail.com>
2026-04-06 19:08:22 +0500

    Added project

---
+++ DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBOperationsWithEFCoreApp.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+    }
+}
