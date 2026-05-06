a99d4e5b6ab21ee4fd5109fe7da72ba03ed83dc6
unsafareed <Unsafareed123@gmail.com>
2026-05-06 18:43:30 +0500

    testing

--- DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Controllers/CurrencyController.cs
+++ DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Controllers/CurrencyController.cs
@@ -62,7 +62,7 @@ namespace DBOperationsWithEFCoreApp.Controllers
             //var result = await (from currencies in _appDBContext.Currencies
             //                    select currencies).ToListAsync();
 
-            return Ok(result);
+            return Ok(result); abcd
         }
 
         [HttpPost("all")]
